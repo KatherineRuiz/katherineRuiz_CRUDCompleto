1b4471b84590b829da690976b5c16d8387886bba
KatherineRuiz <20250409@ricaldone.edu.sv>
2026-07-09 11:25:54 -0600

    Valores de conexion configurados

--- src/main/java/SystemITR/JosueGuinea2A/Departamentos/Controller/DepartamentoController.java
+++ src/main/java/SystemITR/JosueGuinea2A/Departamentos/Controller/DepartamentoController.java
@@ -15,6 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/departamentos")
 public class DepartamentoController {
 
+    /**Aqui estamos Inyectando la capa service sobre el controller (siempre debe hacerse)*/
+    private final DepartamentosService service;
+    public DepartamentoController(DepartamentosService service) {
+        this.service = service;
+    }
+
     /**
      * Nuevo recursos : Ingresar información -> POST
      * Obtener recursos: GET
@@ -22,13 +28,14 @@ public class DepartamentoController {
      * Eliminar recursos: DELETE
      */
     @PostMapping
-    public ResponseEntity<ApiResponse<DepartamentoDTO>> nuevoDepartamento(@Valid @RequestBody DepartamentoDTO json){
+    public ResponseEntity<ApiResponse<DepartamentoDTO>> nuevoDepartamento(@Valid @RequestBody DepartamentoDTO json){ //Este json viene sin id, solo con los datos
         try{
-            ApiResponse<DepartamentoDTO> respuesta = new ApiResponse<>(true, "Datos ingresados exitosamente", json);
+            DepartamentoDTO dto = service.nuevoDepartamento(json);//nuevoDepartamento me devuelve un DepartamentoDTO que es la respuesta de la base
+            ApiResponse<DepartamentoDTO> respuesta = new ApiResponse<>(true, "Datos ingresados exitosamente", dto);//Aquí el jason ya viene con el id de la base y los datos que se ingresaron
             return ResponseEntity.ok(respuesta);
         }catch (Exception e){
             e.printStackTrace();
-            ApiResponse<DepartamentoDTO> respuesta = new ApiResponse<>(false, "El proceso no se pudo completar", json);
+            ApiResponse<DepartamentoDTO> respuesta = new ApiResponse<>(false, "El proceso no se pudo completar", json); //Pasa cuando el dto no recibe nada
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(respuesta);
         }
     }
